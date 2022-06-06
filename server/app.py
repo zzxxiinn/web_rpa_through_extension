4fc69d505d876e1fd3bab4b1aa87189ec262e138
张鑫 <zhangxin8344@gmail.com>
2022-06-06 19:04:36 +0800

    update actions

--- server/app.py
+++ server/app.py
@@ -2,15 +2,40 @@ import json
 import time
 from asyncio import Queue
 
-from sanic import Sanic, text, Request
+import sanic
+from sanic import Sanic, Request
 
 from server.utils.get_browser_path import open_browser
-from web_module import open_page, click_by_xpath, close_page
+from web_module import (
+    browser_open,
+    browser_close,
+    browser_close_all,
+    browser_reload,
+    browser_negative,
+    browser_go_back,
+    browser_go_forward,
+    browser_get_tab,
+    browser_get_tabs,
+    element_click,
+    element_set_input,
+    scroll
+)
 
 task_map = {
-    'openPage': open_page,
-    'clickByXpath': click_by_xpath,
-    'closePage': close_page
+    'browser_open': browser_open,
+    'browser_close': browser_close,
+    'browser_close_all': browser_close_all,
+    'browser_reload': browser_reload,
+    'browser_negative': browser_negative,
+    'browser_go_back': browser_go_back,
+    'browser_go_forward': browser_go_forward,
+    'browser_get_tab': browser_get_tab,
+    'browser_get_tabs': browser_get_tabs,
+    'element_click': element_click,
+    'element_set_input': element_set_input,
+    # 'select': select,
+    # 'cancel_select': cancel_select,
+    'scroll': scroll
 }
 
 SUCC = 'SUCCESS'
@@ -45,30 +70,87 @@ async def feed(request: Request, ws):
     while True:
         try:
             task_data = await request.app.ctx.task_queue.get()
-            task_result = await task_map[task_data['directive']](ws, **task_data['data'])
+            data = task_data.get('data', {})
+            task_result = await task_map[task_data['directive']](ws, **data)
             request.app.ctx.task_result_queue.put_nowait(task_result)
         except Exception as e:
             print(e)
+            # todo put failed result back to queue
 
 
 @app.get("/")
 async def hello_world(request):
     test_url = "https://www.baidu.com/"
     open_browser()
-    status, task_result = await run_task({"directive": "openPage", "data": {"url": test_url}})
+    status, task_result = await run_task({"directive": "browser_open", "data": {"url": test_url}})
     if status == SUCC:
         # data: dict = task_result['res']
-        data = {'url': test_url, 'xpath': '//*[@id="hotsearch-content-wrapper"]/li[1]/a'}
-        status, task_result = await run_task({"directive": "clickByXpath", "data": data})
+        # data = {'url': test_url, 'xpath': '//*[@id="hotsearch-content-wrapper"]/li[1]/a'}
+        await run_task({"directive": "element_click", "data": {
+            "tab_id": task_result['res']['tabId'],
+            "selector": "#kw",
+        }})
 
-    return text(json.dumps(task_result))
+        await run_task({"directive": "element_set_input", "data": {
+            "tab_id": task_result['res']['tabId'],
+            "selector": "#kw",
+            "value": "raid0"
+        }})
+
+        await run_task({"directive": "element_click", "data": {
+            "tab_id": task_result['res']['tabId'],
+            "selector": "#su",
+        }})
+
+    return sanic.json(task_result)
+
+
+@app.get("/login_metis")
+async def hello_world(request):
+    test_url = "https://www.meinenghua.com/user/login?redirect=https%3A%2F%2Fmetis.meinenghua.com"
+    open_browser()
+    status, task_result = await run_task({"directive": "browser_open", "data": {"url": test_url}})
+    if status == SUCC:
+        time.sleep(2)
+
+        status, task_result = await run_task({"directive": "element_set_input", "data": {
+            "tab_id": task_result['res']['tabId'],
+            "selector": "#email",
+            "value": "xin.zhang@meinenghua.com"
+        }})
+
+        time.sleep(1)
+
+        status, task_result = await run_task({"directive": "element_set_input", "data": {
+            "tab_id": task_result['res']['tabId'],
+            "selector": "#password",
+            "value": "123456"
+        }})
+
+        time.sleep(1)
+
+        status, task_result = await run_task({"directive": "element_click", "data": {
+            "tab_id": task_result['res']['tabId'],
+            "selector": "#userLayout > div > div.main > div.main > form > div:nth-child(4) > div > div > span > button"
+        }})
+
+    return sanic.json(task_result)
+
+
+@app.get('/close_all')
+async def browser_close_all(request):
+    status, task_result = await run_task({"directive": 'browser_close_all'})
+    return sanic.json({
+        "status": status,
+        "result": task_result
+    })
 
 
 @app.get("/open_and_close_tab")
 async def open_and_close_tab(request):
     test_url = "https://www.baidu.com/"
     print('step 1: open page')
-    status, task_result = await run_task({"directive": "openPage", "data": {"url": test_url}})
+    status, task_result = await run_task({"directive": "browser_open", "data": {"url": test_url}})
     if status == SUCC:
         print('step 2: close page')
         data = {
@@ -76,9 +158,9 @@ async def open_and_close_tab(request):
             'tab_id': task_result['res']['tabId']
         }
         time.sleep(1)
-        status, task_result = await run_task({"directive": "closePage", "data": data})
+        status, task_result = await run_task({"directive": "browser_close", "data": data})
 
-    return text(json.dumps(task_result))
+    return sanic.json(task_result)
 
 
 if __name__ == '__main__':
--- server/web_module.py
+++ server/web_module.py
@@ -1,26 +1,95 @@
 import json
 
-task_list = [
-    {"directive": "openPage", "data": {"url": ""}},
-    {"directive": "closePage", "data": {"url": "", "tabId": ""}},
-    {"directive": "setInputValue", "data": {"xpath": "", "tabId": "", "url": "", "value": ""}},
-    {"directive": "clickByXpath", "data": {"url": "", "tabId": "", "xpath": "this.xpath"}}
-]
 
-
-async def open_page(ws, url):
-    await ws.send(json.dumps({"directive": "browser.open", "data": {"url": url}}))
+async def ws_swap_msg(ws, msg):
+    await ws.send(json.dumps(msg))
     result = await ws.recv()
     return result
 
 
-async def click_by_xpath(ws, url, tab_id, xpath):
-    await ws.send(json.dumps({"directive": "clickElement", "data": {"url": url, "tabId": tab_id, "xpath": xpath}}))
-    result = await ws.recv()
-    return result
+async def browser_open(ws, url):
+    return await ws_swap_msg(ws, {"directive": "browser.open", "data": {"url": url}})
 
 
-async def close_page(ws, url, tab_id):
-    await ws.send(json.dumps({"directive": "browser.close", "data": {"url": url, "tabId": tab_id}}))
-    result = await ws.recv()
-    return result
+async def browser_close(ws, url, tab_id):
+    return await ws_swap_msg(ws, {"directive": "browser.close", "data": {"url": url, "tabId": tab_id}})
+
+
+async def browser_close_all(ws):
+    return await ws_swap_msg(ws, {"directive": "browser.closeAll"})
+
+
+async def browser_reload(ws, tab_id):
+    return await ws_swap_msg(ws, {"directive": "browser.reload", "data": {"tabId": tab_id}})
+
+
+async def browser_go_forward(ws, tab_id):
+    return await ws_swap_msg(ws, {"directive": "browser.goForward", "data": {"tabId": tab_id}})
+
+
+async def browser_go_back(ws, tab_id):
+    return await ws_swap_msg(ws, {"directive": "browser.goBack", "data": {"tabId": tab_id}})
+
+
+async def browser_negative(ws, tab_id, url):
+    return await ws_swap_msg(ws, {
+        "directive": "browser.negative",
+        "data": {
+            "tabId": tab_id,
+            "url": url
+        }
+    })
+
+
+async def browser_get_tab(ws, url):
+    return await ws_swap_msg(ws, {
+        "directive": "browser_getTab",
+        "data": {
+            "url": url
+        }
+    })
+
+
+async def browser_get_tabs(ws):
+    return await ws_swap_msg(ws, {"directive": "browser_getTabs"})
+
+
+async def element_set_input(ws, tab_id, xpath='', selector='', value=''):
+    return await ws_swap_msg(ws, {
+        "directive": "element.setInput",
+        "data": {
+            "tabId": tab_id,
+            "xpath": xpath,
+            "selector": selector,
+            "value": value
+        }
+    })
+
+
+async def element_click(ws, tab_id, xpath='', selector=''):
+    return await ws_swap_msg(ws, {
+        "directive": "element.click",
+        "data": {
+            "tabId": tab_id,
+            "xpath": xpath,
+            "selector": selector
+        }
+    })
+
+
+async def select(ws):
+    return await ws_swap_msg(ws, {"directive": "select"})
+
+
+async def cancel_select(ws):
+    return await ws_swap_msg(ws, {"directive": "cancelSelect"})
+
+
+async def scroll(ws, url, scroll_x=float('Infinity')):
+    return await ws_swap_msg(ws, {
+        "directive": "scroll",
+        "data": {
+            "url": url,
+            "scrollX": scroll_x
+        }
+    })
